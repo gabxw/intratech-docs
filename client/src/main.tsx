65e8075806773b625abd3cdd7f31f6aed51cf91e
gabriel <gabriel.jmaciel13@gmail.com>
2026-01-05 12:14:04 -0300

    teste

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+
+if (redirect) {
+  history.replaceState(null, '', redirect.replace(location.origin, ''));
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
