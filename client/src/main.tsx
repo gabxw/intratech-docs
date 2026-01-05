18c302c76beac41ec80ed262cc46051a6db6ba55
gabriel <gabriel.jmaciel13@gmail.com>
2026-01-05 12:28:08 -0300

    teste

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// If the 404 page redirected here (GitHub Pages), restore the original URL
 const redirect = sessionStorage.redirect;
 delete sessionStorage.redirect;
 
 if (redirect) {
-  history.replaceState(null, '', redirect.replace(location.origin, ''));
+  history.replaceState(null, "", redirect.replace(location.origin, ""));
 }
 
+// Mount the app once
 createRoot(document.getElementById("root")!).render(<App />);
